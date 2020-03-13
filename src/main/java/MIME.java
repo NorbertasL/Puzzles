3084c2fdbf564fe24afeee7a6d0de22763d41f59
NorbertasL <nlapenas@gmail.com>
2020-03-13 11:54:33 +0000

    Cleaned up MIME Solution code with lastIndexOf and getOrDefault

--- src/main/java/MIME.java
+++ src/main/java/MIME.java
@@ -18,34 +18,9 @@ class MIME {
         for (int i = 0; i < Q; i++) {
             String FNAME = in.nextLine(); // One file name per line.
             System.err.println(FNAME);
-            String ext = null;
-            try{
-                if(FNAME.charAt(FNAME.length()-1) == '.'){
-                    throw new ArrayIndexOutOfBoundsException("Bad extension");
-                }
-                // "." is a special char so we need to use \\ or enclose it in []
-                String [] split = FNAME.split("[.]");
-                ext = split.length > 2 ? split[split.length-1] : split[1];
-
-            }catch(ArrayIndexOutOfBoundsException e){
-                System.out.println("UNKNOWN");
-                continue;
-            }
-
-            String mimeType = mimePairs.get(ext);
-            if(mimeType == null){
-                System.out.println("UNKNOWN");
-            }
-            else{
-                System.out.println(mimeType);
-            }
+            int lastDotIndex = FNAME.lastIndexOf(".");
+            String extension = lastDotIndex == -1 ? "" : FNAME.substring(lastDotIndex + 1);
+            System.out.println(mimePairs.getOrDefault(extension, "UNKNOWN"));
         }
-
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
-
-
-        // For each of the Q filenames, display on a line the corresponding MIME type. If there is no corresponding type, then display UNKNOWN.
-
     }
 }
