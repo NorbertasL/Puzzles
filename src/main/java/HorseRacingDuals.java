79bdf978b3499fac88deb45fa6e9a6be0279ba8d
NorbertasL <nlapenas@gmail.com>
2020-04-23 13:44:23 +0100

    Horse-racing Duals solution optimised for big arrays too.

--- src/main/java/HorseRacingDuals.java
+++ src/main/java/HorseRacingDuals.java
@@ -1,25 +1,29 @@
 import java.util.*;
 
+/**
+ * Turns out Arrays.sort() is really fast/optimised.Should use it more often.
+ */
 public class HorseRacingDuals {
     public static void main(String args[]) {
+        long startTimeStamp = System.currentTimeMillis();
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-        System.err.println("Size:"+N);
+        System.err.println("Array Size:"+N);
         int [] horses = new int[N];
         for (int i = 0; i < N; i++) {
             horses[i] = in.nextInt();
         }
-        int dif = -1;
-        for(int i = 0; i<N-1; i++){
-            for(int j = i+1; j<N; j++){
-                int strDif = Math.abs(horses[i]-horses[j]);
-                if(dif>strDif || dif == -1){
-                    dif = strDif;
-                }
+        System.err.println("Array gen time:"+ (System.currentTimeMillis() - startTimeStamp)+"ms");
+        long lastStamp = System.currentTimeMillis();
+        Arrays.sort(horses);
+        System.err.println("Array Sort time:"+ (System.currentTimeMillis() - lastStamp)+"ms");
+        int dif = horses[1]-horses[0];
+        for(int i = 1; i<N-1; i++){
+            if(horses[i+1]-horses[i] < dif){
+                dif = horses[i+1]-horses[i];
             }
         }
         System.out.println(dif);
-
-
+        System.err.println("Total computation time:"+(System.currentTimeMillis()-startTimeStamp)+"ms");
     }
 }
